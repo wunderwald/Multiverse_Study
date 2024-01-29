18192e5694be08e436ec0544334102e39004fe77
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-29 20:14:11 +0100

    converted polyV

---
+++ RSA_Drew_Py/poly_v.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def PolyV(degree, num_points):
+
+    """
+    Generates polynomial values based on the given degree and number of points
+
+    Parameters:
+    degree (int): Degree of the polynomial
+    num_points (int): Number of points for which polynomial values are generated
+
+    Returns:
+    array: Array of polynomial values
+    """
+    
+    polyvals = np.zeros(num_points)
+
+    if degree < 2 or degree > 5:
+        raise ValueError("Degree must be between 2 and 5")
+
+    if degree + 1 > num_points:
+        raise ValueError("Number of points must be greater than degree + 1")
+
+    # Adjusting num_points to the closest even number less than num_points
+    if num_points % 2 != 0:
+        num_points -= 1
+
+    n3 = num_points // 2
+    a0 = num_points
+    a2 = a4 = a6 = a8 = 0
+
+    for i in range(1, n3 + 1):
+        aI = i
+        a2 += 2 * aI ** 2
+        a4 += 2 * aI ** 4
+        a6 += 2 * aI ** 6
+        a8 += 2 * aI ** 8
+
+    aJ = -n3 - 1
+    if degree > 3:
+        den = 1.0 / (a0 * a4 * a8 + 2 * a2 * a4 * a6 - a4 ** 3 - a0 * a6 ** 2 - a2 ** 2 * a8)
+        c1 = a4 * a8 - a6 ** 2
+        c2 = a4 * a6 - a2 * a8
+        c3 = a2 * a6 - a4 ** 2
+
+        for i in range(num_points):
+            aJ += 1
+            polyvals[i] = den * (c1 + c2 * aJ ** 2 + c3 * aJ ** 4)
+    else:
+        den = 1.0 / (a0 * a4 - a2 ** 2)
+        for i in range(num_points):
+            aJ += 1
+            aJ2 = aJ ** 2
+            polyvals[i] = den * (a4 - aJ2 * a2)
+
+    # symmetrize the polynomial values
+    for i in range(n3 + 1, num_points):
+        polyvals[i] = polyvals[num_points - i]
+
+    # normalize the polynomial values
+    polyvals /= np.sum(polyvals)
+
+    return polyvals
