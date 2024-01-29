d4df4d165db64b89f5cd9c350b0e4c43786a2dec
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-29 19:51:21 +0100

    converted dyad rsa to csv

---
+++ RSA_Drew_Py/dyad_rsa_to_csv_file.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import os
+
+def dyad_rsa_to_csv_file(mother_rsa, infant_rsa, name, dyad_id, output_dir):
+
+    """
+    writes dyad rsa data to a csv file.
+
+    params:
+    mother_rsa (array-like), infant_rsa (array-like): rsa data for the infant and mother
+    name (str): the name of the specific rsa data
+    dyad_id (str): dyad identifiers
+    output_dir (str): directory where the CSV file will be saved
+
+    Returns:
+    str: Path of the saved CSV file, or an error message if lengths do not match
+    """
+
+    if len(mother_rsa) != len(infant_rsa):
+        return "! Lengths of mother and infant RSA do not match. Output cannot be written."
+
+    df = pd.DataFrame({'motherRsa': mother_rsa, 'infantRsa': infant_rsa})
+    
+    file_path = os.path.join(output_dir, f"{dyad_id}_{name}.csv")
+
+    df.to_csv(file_path, index=False, header=True, sep=';')
+
+    return file_path
+
