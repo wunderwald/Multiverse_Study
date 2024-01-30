968d2cd656483525735f7ab406c1d554fc05caeb
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-30 13:26:43 +0100

    added concept for test parameter generator

---
+++ IBI_Generator/parameter_generator.py
@@ -0,0 +1,11 @@
+def test_parameter_generator():
+    '''
+    Idea:
+    - Frequency bands:
+        - frequency ranges for each of the 16 bands
+        - randomly pick a frequency for each band
+    - Frequency influence:
+        - randomly pick a factor for each frequency band
+        - make sure that the sum of factors exeeds a given threshold
+    '''
+    return
