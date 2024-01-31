194e5f19843bda1af80ddf8d8bf127a5e5178934
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-31 18:27:19 +0100

    added parameter ranges

--- ML/main.py
+++ ML/main.py
@@ -1,27 +1,32 @@
 import ibi_generator as ibi
 import rsa_drew as rsa
+import numpy as np
+
+# define constants for IBI generation
+RECORDING_TIME = 300
+NUM_VLF_BANDS = 4
+NUM_LF_BANDS = 6
+NUM_HF_BANDS = 6
 
 # define parameter ranges for IBI generation
 PARAM_RANGES = {
-    'freq_vlf': [],
-    'freq_lf': [],
-    'freq_hf': [],
-    'weights_vlf': [],
-    'weights_lf': [],
-    'weights_hf': [],
-    'phase_shift': [],
-    'base_ibi_adult': [],
-    'base_ibi_infant': []
+    'freq_vlf': [0.01, 0.04],
+    'freq_lf': [0.04, 0.15],
+    'freq_hf': [0.15, 0.4],
+    'weights_vlf': [0.015, 1.0],
+    'weights_lf': [0.004, 0.4],
+    'weights_hf': [0.002, 0.2],
+    'phase_shift': [0, 2 * np.pi],
+    'base_ibi_adult': [650, 750],
+    'base_ibi_infant': [450, 550]
 }
+
+# define constants for parameter optimization
+TARGET_ZLC = 300
 
 # define hyper-parameter ranges for parameter optimization
 HYPERPARAM_RANGES = {}
 
-# define constants for IBI generation
-RECORDING_TIME = 300.
-
-# define constants for parameter optimization
-TARGET_ZLC = 300.
 
 # define objective function
 def obj_rsa_sync_zlc(params: dict, target_zlc: float):
