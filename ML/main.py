9db509a96ea42ca1c200db32f8670a360d21c3c1
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-31 10:26:35 +0100

    made test script

--- ML/main.py
+++ ML/main.py
@@ -4,15 +4,15 @@ import numpy as np
 
 RECORDING_TIME = 300
 
-zlcs = np.array([])
+zlcs = []
 
-for i in range(10000):
+for i in range(100):
     adult_params, infant_params = ibi.generate_dyad_ibi_params()
     adult_ibi, infant_ibi = ibi.generate_dyad_ibi(RECORDING_TIME, adult_params, infant_params)
 
     zlc, ccf = rsa.rsa_synchrony(adult_ibi, infant_ibi)
 
-    zlcs = np.append(zlcs, zlc)
+    zlcs.append(zlc)
 
 print(f"Min: {np.min(zlcs)}")
 print(f"Max: {np.max(zlcs)}")
