a31b19268633b541b41dd1c659922f60fb7df267
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-31 09:28:51 +0100

    added setup.py

---
+++ RSA_Drew/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rsa',
+    version='0.4',
+    packages=find_packages(),
+    description='Calculate RSA Synchrony',
+    author='Moritz Wunderwald',
+    author_email='code@moritzwunderwald.de'
+)
