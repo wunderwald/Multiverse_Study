401cc92b92d1a62177f7af9661ea4bf926beb3df
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-31 09:02:31 +0100

    added build

---
+++ IBI_Generator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ibi_gen',
+    version='0.4',
+    packages=find_packages(),
+    description='Generate IBI sequen',
+    author='Moritz Wunderwald',
+    author_email='code@moritzwunderwald.de'
+)
