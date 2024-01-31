47ce8478d207c753871691e2abe40bacda687eff
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-01-31 18:23:17 +0100

    added documentation

--- ML/main.py
+++ ML/main.py
@@ -24,19 +24,39 @@ RECORDING_TIME = 300
 TARGET_ZLC = 300
 
 # define objective function
-def obj_rsa_sync_zlc(params, target_zlc):
+def obj_rsa_sync_zlc(params: dict, target_zlc: float):
+    '''
+    Objective function for calculating RSA synchrony using Drew's algorithm. 
+    Synchrony is measured using the zero-lag coefficient (ZLC).
+    The error is calculated as the absolute difference between the ZLC and a target ZLC.
+
+    Parameters:
+    - params (dict): key-value pairs for the 98 parameters for dyad IBI generator (see README for details)
+    - target_zlc (float): zero lag coefficient value that is the optimization target 
+
+    Returns:
+    - err (float): the absolute difference between calculated and target ZLC, float('inf') on error
+    '''
+
+    # validate parameters
+    # TODO test if parameters are in range, return float('inf') on fail
 
     # extract parameters
     adult_params = params['adult']
     infant_params = params['adult']
 
-    # generate IBIs
-    adult_ibi, infant_ibi = ibi.generate_dyad_ibi(RECORDING_TIME, adult_params, infant_params)
+    try:
+        # generate IBIs
+        adult_ibi, infant_ibi = ibi.generate_dyad_ibi(RECORDING_TIME, adult_params, infant_params)
 
-    # calculate synchrony
-    zlc, _ = rsa.rsa_synchrony(adult_ibi, infant_ibi)
+        # calculate synchrony
+        zlc, _ = rsa.rsa_synchrony(adult_ibi, infant_ibi)
 
-    # calculate error
-    err = abs(zlc - target_zlc)
+        # calculate error
+        err = abs(zlc - target_zlc)
 
-    return err
+        return err
+    
+    # return infinity on error
+    except ValueError:
+        return float('inf')
