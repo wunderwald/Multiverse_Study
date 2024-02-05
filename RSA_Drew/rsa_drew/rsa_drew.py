a755399ace308a86439b44dff03f7e3b14fd06df
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-02-05 13:37:16 +0100

    added step export

--- RSA_Drew/rsa_drew/rsa_drew.py
+++ RSA_Drew/rsa_drew/rsa_drew.py
@@ -1,13 +1,17 @@
 import pkg_resources
 import numpy as np
 import pandas as pd
+import os
+import time
 from scipy.signal import convolve, detrend
 from scipy.interpolate import interp1d
 from .resampled_ibi_ts import resampled_IBI_ts
 from .poly_filter_data_2011 import poly_filter_data_2011
 from .sliding_window import sliding_window_log_var
+from .number_to_csv import number_to_csv
+from .arr_to_csv import arr_to_csv
 
-def rsa_synchrony(mother_ibi, infant_ibi):
+def rsa_synchrony(mother_ibi, infant_ibi, export_steps=False):
     """
     This function calculates the zero lag coefficient and the full cross-correlation function (ccf)
     between detrended, logarithmic variances of filtered Respiratory Sinus Arrhythmia (RSA) signals 
@@ -16,6 +20,7 @@ def rsa_synchrony(mother_ibi, infant_ibi):
     Parameters:
     - mother_ibi (array-like): list of mother's IBIs in ms
     - infant_ibi (array-like): list of infant's IBIs in ms
+    - export_steps (bool): export data at each processing step
     
     Returns:
     - zeroLagCoefficient (float): The zero lag coefficient in the cross-correlation function, indicating the degree of synchrony at zero time lag between the RSA signals.
@@ -24,6 +29,13 @@ def rsa_synchrony(mother_ibi, infant_ibi):
     Raises:
     - ValueError: If the length of filtered RSA data is insufficient for further processing.
     """
+    # export directories for export_steps
+    export_dir = './log'
+    if not os.path.exists(export_dir):
+        os.makedirs(export_dir)
+    export_ts = f"{time.time()}".replace(".", "_")
+    export_subdir = os.path.join(export_dir, export_ts)
+    os.makedirs(export_subdir)
 
     # load filters
     filt_M_path = pkg_resources.resource_filename('rsa_drew', 'adult_rsa_5Hz_cLSq.csv')
@@ -31,9 +43,14 @@ def rsa_synchrony(mother_ibi, infant_ibi):
     filt_M = pd.read_csv(filt_M_path).to_numpy().flatten()
     filt_I = pd.read_csv(filt_I_path).to_numpy().flatten()
 
-    # resamle IBI data to 5Hz
+    # resample IBI data to 5Hz
     r_M = resampled_IBI_ts(mother_ibi, 5, False)
     r_I = resampled_IBI_ts(infant_ibi, 5, False)
+
+    # optionally export data
+    if export_steps:
+        arr_to_csv(r_M[:, 1], 'r_M', export_ts, export_subdir)
+        arr_to_csv(r_I[:, 1], 'r_I', export_ts, export_subdir)
 
     # get RSA/BPM and filter RSA
     RSA_M, BPM_M = poly_filter_data_2011(r_M[:, 1], 51, True) 
@@ -41,6 +58,14 @@ def rsa_synchrony(mother_ibi, infant_ibi):
 
     RSA_I, BPM_I = poly_filter_data_2011(r_I[:, 1], 51, True)
     RSA_I_filt = convolve(RSA_I, filt_I, mode='valid')
+
+    # optionally export data
+    if export_steps:
+        arr_to_csv(RSA_M, 'RSA_M', export_ts, export_subdir)
+        arr_to_csv(RSA_I, 'RSA_I', export_ts, export_subdir)
+        arr_to_csv(RSA_M_filt, 'RSA_M_filt', export_ts, export_subdir)
+        arr_to_csv(RSA_I_filt, 'RSA_I_filt', export_ts, export_subdir)
+        
 
     # interpolate filtered RSA data
     if len(RSA_M_filt) < 2 or len(RSA_I_filt) < 2:
@@ -50,6 +75,11 @@ def rsa_synchrony(mother_ibi, infant_ibi):
     RSA_M_filt_intpl = f(np.linspace(0, len(RSA_M_filt) - 1, len(r_M)))
     f = interp1d(np.arange(len(RSA_I_filt)), RSA_I_filt)
     RSA_I_filt_intpl = f(np.linspace(0, len(RSA_I_filt) - 1, len(r_M)))
+
+    # optionally export data
+    if export_steps:
+        arr_to_csv(RSA_M_filt_intpl, 'RSA_M_filt_intpl', export_ts, export_subdir)
+        arr_to_csv(RSA_I_filt_intpl, 'RSA_I_filt_intpl', export_ts, export_subdir)
 
     # calculate log of variance with sliding window
     window_size = 74  # 15 seconds window at 5 Hz sampling rate
@@ -61,9 +91,19 @@ def rsa_synchrony(mother_ibi, infant_ibi):
     lv_RSA_M_fif = lv_RSA_M_fif_raw[:min_length]
     lv_RSA_I_fif = lv_RSA_I_fif_raw[:min_length]
 
+    # optionally export data
+    if export_steps:
+        arr_to_csv(lv_RSA_M_fif, 'lv_RSA_M_fif', export_ts, export_subdir)
+        arr_to_csv(lv_RSA_I_fif, 'lv_RSA_I_fif', export_ts, export_subdir)
+
     # detrend RSA signals
     lv_RSA_M_fif_detrended = detrend(lv_RSA_M_fif)
     lv_RSA_I_fif_detrended = detrend(lv_RSA_I_fif)
+
+    # optionally export data
+    if export_steps:
+        arr_to_csv(lv_RSA_M_fif_detrended, 'lv_RSA_M_fif_detrended', export_ts, export_subdir)
+        arr_to_csv(lv_RSA_I_fif_detrended, 'lv_RSA_I_fif_detrended', export_ts, export_subdir)
 
     # calculate full cross-correlation
     ccf_full = np.correlate(lv_RSA_M_fif_detrended, lv_RSA_I_fif_detrended, mode='full')
@@ -75,7 +115,15 @@ def rsa_synchrony(mother_ibi, infant_ibi):
     num_lags = (len(ccf_full) - 1) // 2
     ccf = ccf_full[num_lags - maxlag : num_lags + maxlag + 1]
 
+    # optionally export data
+    if export_steps:
+        arr_to_csv(ccf, 'ccf', export_ts, export_subdir)
+
     # read zero lag coefficient
     zeroLagCoefficient = ccf[len(ccf) // 2]
+
+    # optionally export data
+    if export_steps:
+        number_to_csv(zeroLagCoefficient, 'zeroLagCoefficient', export_ts, export_subdir)
 
     return zeroLagCoefficient, ccf
