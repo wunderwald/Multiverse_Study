e36bf3f9e7d2548bcb1b558257f0d334f0e8eb67
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-02-05 13:05:11 +0100

    added RSA test script

---
+++ ML/test_rsa.py
@@ -0,0 +1,41 @@
+
+import os
+import shutil
+import pandas as pd
+import rsa_drew as rsa
+
+# I/O dirs
+inputDir = '../RSA_Drew/rsa_drew/dyad_ibi_data'
+outputDir = "./dyad_rsa_data"
+
+# create output dir if it doesn't exist yet
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
+# clear output dir
+for f in os.listdir(outputDir):
+    file_path = os.path.join(outputDir, f)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+    elif os.path.isdir(file_path):
+        shutil.rmtree(file_path)
+
+# get input dirs
+dyads = [d for d in os.listdir(inputDir) if os.path.isdir(os.path.join(inputDir, d))]
+
+# process dyads
+for dyad in dyads:
+    print(f"## Processing {dyad}")
+
+    # paths of IBI files
+    motherPath = os.path.join(inputDir, dyad, "ECG1", "ibi_ms.csv")
+    infantPath = os.path.join(inputDir, dyad, "ECG2", "ibi_ms.csv")
+
+    # load IBI data
+    M = pd.read_csv(motherPath)['ms'].to_numpy().flatten()
+    I = pd.read_csv(infantPath)['ms'].to_numpy().flatten()
+
+    zcl, ccf = rsa.rsa_synchrony(M, I)
+
+    print(zcl)
+    
