7c0339451e3942cb4bd45609b7b0d1b586d5f63d
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-02-09 13:41:00 +0100

    added todo

--- ML/main.py
+++ ML/main.py
@@ -1,1 +1,5 @@
-import genetic
+import genetic as gen
+
+# goal: have one function gen.optimize_genetic(*hyperparams) that returns given number of best individuals / arr induvidual with fitness under threshold
+
+# hyperparams: popsize, maxgenerations, fitness_threshold (threshold that implies convergence, algorithm stops here), distance_metric, crossover_method, selection, mutation
