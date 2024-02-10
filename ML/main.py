018436e7b456712df94d124e5ac294d8045a8302
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-02-10 17:19:29 +0100

    update hyperparams

--- ML/main.py
+++ ML/main.py
@@ -2,13 +2,13 @@ import numpy as np
 import genetic as gen
 
 # set hyper-parameters
-POPULATION_SIZE = 100
+POPULATION_SIZE = 80
 MAX_NUM_GENERATIONS = 100
-FITNESS_THRESH = 0.3
+FITNESS_THRESH = 0.5
 DISTANCE_METRIC = 'log'
-CROSSOVER_METHOD = None
-MUTATION_RATE = .1
-MUTATION_SCALE = .1
+CROSSOVER_METHOD = 'blend'
+MUTATION_RATE = .2
+MUTATION_SCALE = .15
 TARGET_ZLC = 300.0
 
 # Output parameters
@@ -40,4 +40,6 @@ Open for experimentation:
 - expand weight ranges
 - randomize crossover method for each crossover
 - add parrallelization: evaluate individuals
+- apply threshold not to min (bc thats probably an outlier) but to quantile or smth
+- specialise mutation to parameter characteristics
 '''
