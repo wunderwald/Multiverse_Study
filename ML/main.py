2668a7a33072c4217fcad60fc6cf797fdf6fdbfe
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-02-21 11:32:31 +0100

    update hyperparameters

--- ML/main.py
+++ ML/main.py
@@ -1,11 +1,11 @@
 '''
-Open for experimentation / TODO:
-
+TODO
+Open for experimentation:
 - expand weight ranges?
-- randomize crossover method for each crossover (add this as CROSSOVER_METHOD='shuffle', give tendency towards 'abs')
 
 Create (parallel) batch optimization (run a lot of optimizations simultaneously)
 - Stop on convergence (i.e. 30 generations with same best fitness)
+- add a bit of hyperparameter randomness for each optimization
 - Store individuals that are close to max fitness in database
 '''
 
@@ -13,12 +13,12 @@ import numpy as np
 import genetic as gen
 
 # set hyper-parameters
-POPULATION_SIZE = 100
+POPULATION_SIZE = 200
 MAX_NUM_GENERATIONS = 200
 DISTANCE_METRIC = 'abs'
 CROSSOVER_METHOD = 'shuffle'
 MUTATION_RATE = .1
-MUTATION_SCALE = .8
+MUTATION_SCALE = .9
 SELECT_PARENTS_METHOD = 'sus'
 PARENT_RATIO = 0.1
 TARGET_ZLC = 300.0
