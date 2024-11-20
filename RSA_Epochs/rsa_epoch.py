858a56e8ab3788caf8f52b71742ddb3c30ad5d7b
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2024-11-20 17:23:14 +0100

    added differentiation between adult-adult and adult-infant dyads

--- RSA_Epochs/rsa_epoch.py
+++ RSA_Epochs/rsa_epoch.py
@@ -3,7 +3,7 @@ import scipy.stats
 import neurokit2 as nk2
 from neurokit2.hrv.hrv_rsa import _hrv_rsa_pb
 from resample import resample_ibi
-from rsa_drew import rsa_magnitude_adult
+from rsa_drew import rsa_magnitude_adult, rsa_magnitude_infant
 
 # --------------------------------
 # HELPERS
@@ -88,18 +88,20 @@ def rsa_porges_bohrer(ibi_ms):
 def rsa_drew_magnitude(ibi_ms):
     return rsa_magnitude_adult(ibi_ms)
 
-def rsa_per_epoch(ibi_ms, epoch_length_ms, rsa_method):
+def rsa_per_epoch(ibi_ms, epoch_length_ms, rsa_method, age_type='adult'):
     '''
     Calculates epoch-based RSA in epochs of a set length.
 
     RSA can be estimated using different techniques:
-    - hf_hrv: high-frequency heart-rate variability is interpreted as RSA
-    - drew: Drew Abbneys RSA algorithm is calcuated for consecutive, non-overlapping windows (aka epochs) 
+    - high-frequency heart-rate variability is interpreted as RSA
+    - porges_bohrer: porges bohrer algorithm implementation in neurokit2
+    - drew: Drew Abbneys RSA algorithm is calcuated for consecutive, non-overlapping windows (variant/implementation of porges bohrer)
 
     Parameters:
     - ibi_ms (array-like): list of ibi samples in ms
     - epoch_length_ms (float): (max) length of epochs in ms
     - rsa_method (string): options are 'hf_hrv', 'drew', 'porges_bohrer'...
+    - age_type (string): 'adult' (default) or 'infant (only needed for drew RSA)
 
     Returns:
     - epochs (array): list of epoch-based rsa values 
@@ -122,22 +124,34 @@ def rsa_per_epoch(ibi_ms, epoch_length_ms, rsa_method):
             case 'porges_bohrer':
                 rsa_value = rsa_porges_bohrer(epoch)
             case 'drew':
-                rsa_value = rsa_magnitude_adult(epoch)
+                rsa_value = rsa_magnitude_adult(epoch) if age_type == 'adult' else rsa_magnitude_infant(epoch)
         rsa_epochs.append(rsa_value)
     return rsa_epochs
 
-def rsa_epoch_synchrony(ibi_a_ms, ibi_b_ms, epoch_length_ms=30000, rsa_method='hf_hrv', sync_type='pearson_corr'):
+def rsa_epoch_synchrony(ibi_a_ms, ibi_b_ms, epoch_length_ms=30000, rsa_method='hf_hrv', sync_type='pearson_corr', dyad_type='adult_infant'):
     '''
     Calculates epoch based rsa for a pair of ibi sequences. Using a given synchrony measure, a synchrony score is calculated for the pair of rsa epochs.
 
     Parameters:
+    - ibi_a_ms (array-like): ibi signal a in milliseconds
+    - ibi_b_ms (array-like): ibi signal b in milliseconds
+    - epoch_length_ms (int): epoch length in milliseconds
+    - rsa_method (string): method of calculating rsa magnitude. Options: 'drew', 'hf_hrv', 'porges_bohrer'
     - sync_type (string): see options in synchrony()
+    - dyad_type (string): 'adult-infant' (default) or 'adult-adult' dyad (only important for drew RSA).
 
     Returns:
     - synchrony_score (float): the synchrony score
     '''
-    epochs_a = rsa_per_epoch(ibi_a_ms, epoch_length_ms, rsa_method)
-    epochs_b = rsa_per_epoch(ibi_b_ms, epoch_length_ms, rsa_method)
+    # determine age types (only needed for drew RSA)
+    age_type_a = 'adult'
+    age_type_b = 'adult' if dyad_type == 'adult_adult' else 'infant'
+
+    # calculate rsa epochs
+    epochs_a = rsa_per_epoch(ibi_a_ms, epoch_length_ms, rsa_method, age_type_a)
+    epochs_b = rsa_per_epoch(ibi_b_ms, epoch_length_ms, rsa_method, age_type_b)
+
+    # calculeate synchrony between the two rsa epoch lists
     synchrony_score = synchrony(epochs_a, epochs_b, sync_type)
     return synchrony_score
 
